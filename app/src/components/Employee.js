2ec2792aeb06816548ac55e3fbb429ded876cf63
Luke Watson <>
2020-08-26 22:08:56 +0800

    Add earnings timer

--- app/src/components/Employee.js
+++ app/src/components/Employee.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useCallback} from "react";
-import {Contract} from "ethers";
+import {utils, Contract} from "ethers";
 import StreamEmployee from "../build/StreamEmployee.json";
 
 const Employee = (props) => {
@@ -7,25 +7,64 @@ const Employee = (props) => {
 
     const [employeeAddress, setEmployeeAddress] = useState(undefined);
     const [employeeContract, setEmployeeContract] = useState(undefined);
+    const [payPerSecond, setPayPerSecond] = useState(undefined);
+    const [balanceEarned, setBalanceEarned] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [isWorking, setIsWorking] = useState(undefined);
 
     useEffect(() => {
         company.employees(alice).then(setEmployeeAddress)
     }, [alice]);
 
     useEffect(() => {
-        if(undefined !== employeeAddress) {
-            setEmployeeContract(new Contract(employeeAddress, StreamEmployee.abi, provider));
+        if (undefined !== employeeAddress) {
+            setEmployeeContract(new Contract(employeeAddress, StreamEmployee.abi, provider.getSigner()));
         }
     }, [employeeAddress]);
 
+    useEffect(() => {
+        if (undefined !== employeeContract) {
+            employeeContract.payPerSecond().then(setPayPerSecond);
+        }
+    }, [employeeContract]);
+
+    useEffect(() => {
+        if (undefined !== payPerSecond) {
+            const interval = setInterval(() => {
+                setSeconds(seconds => seconds + 1)
+                setBalanceEarned(payPerSecond.mul(seconds))
+            }, 1000)
+
+            return () => clearInterval(interval);
+        }
+    }, [payPerSecond, seconds]);
+
     const startWorking = async () => {
-        employeeContract.payPerHour().then(console.log)
+        await employeeContract.startWorking()
+        await employeeContract.isWorking().then(setIsWorking)
+    }
+
+    const displayWorkingClock = () => {
+        if (isWorking) {
+            return (
+                <div>
+                    Pay per second {payPerSecond.toString()}
+                    Balance {utils.formatEther(balanceEarned.toString())}
+                </div>
+            )
+        }
+        return (
+            <div>
+                Not working
+            </div>
+        )
     }
 
     return (
         <div>
             <p>Contract address: {employeeAddress}</p>
             <input type="submit"
+                   value="Start working"
                    onClick={
                        useCallback(
                            () => startWorking(),
@@ -33,6 +72,8 @@ const Employee = (props) => {
                        )
                    }
             />
+
+            {displayWorkingClock()}
         </div>
     )
 
