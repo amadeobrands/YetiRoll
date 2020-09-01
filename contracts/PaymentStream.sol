1cd563c09e4e6b3d4320554fc26ea1e0423a264b
Luke Watson <>
2020-09-01 17:33:35 +0800

    Remove tie to Sabblier

--- contracts/PaymentStream.sol
+++ contracts/PaymentStream.sol
@@ -7,7 +7,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./lib/Types.sol";
 import "./Sablier.sol";
 
-contract PaymentStream is Sablier {
+contract PaymentStream {
+    using SafeMath for uint;
 
     modifier baseStreamRequirements(address recipient, uint deposit, uint startTime) {
         require(recipient != address(0x00), "stream to the zero address");
@@ -15,6 +16,11 @@ contract PaymentStream is Sablier {
         require(recipient != msg.sender, "stream to the caller");
         require(deposit > 0, "deposit is zero");
         require(startTime >= block.timestamp, "start time before block.timestamp");
+        _;
+    }
+
+    modifier streamExists(uint256 streamId) {
+        require(streams[streamId].isEntity, "Stream does not exist");
         _;
     }
 
@@ -27,9 +33,14 @@ contract PaymentStream is Sablier {
         bool isActive
     );
 
-    using SafeMath for uint;
 
+    mapping(uint256 => Types.Stream) private streams;
     mapping(uint256 => Types.PausableStream) private pausableStreams;
+    uint256 public nextStreamId;
+
+    constructor() public {
+        nextStreamId = 1;
+    }
 
 
     //           _____      _   _
@@ -57,6 +68,31 @@ contract PaymentStream is Sablier {
         );
     }
 
+    function getStream(uint256 streamId)
+    external
+    view
+    streamExists(streamId)
+    returns (
+        address sender,
+        address recipient,
+        uint256 deposit,
+        address tokenAddress,
+        uint256 startTime,
+        uint256 stopTime,
+        uint256 remainingBalance,
+        uint256 ratePerSecond
+    )
+    {
+        sender = streams[streamId].sender;
+        recipient = streams[streamId].recipient;
+        deposit = streams[streamId].deposit;
+        tokenAddress = streams[streamId].tokenAddress;
+        startTime = streams[streamId].startTime;
+        stopTime = streams[streamId].stopTime;
+        remainingBalance = streams[streamId].remainingBalance;
+        ratePerSecond = streams[streamId].ratePerSecond;
+    }
+
 
     function createStream(
         address _recipient,
@@ -64,18 +100,27 @@ contract PaymentStream is Sablier {
         address _tokenAddress,
         uint256 _startTime,
         uint256 _stopTime
-    ) public override
+    ) public
     baseStreamRequirements(_recipient, _deposit, _startTime)
     returns (uint256 streamId) {
         require(isNonZeroLengthStream(_startTime, _stopTime), "Stream must last a least a second");
 
-        uint streamId = super.createStream(
-            _recipient,
-            _deposit,
-            _tokenAddress,
-            _startTime,
-            _stopTime
-        );
+        uint duration = _stopTime.sub(_startTime);
+        uint ratePerSecond = _ratePerSecond(_deposit, duration);
+        require(ratePerSecond > 0, "Rate per second is under 0");
+
+        uint256 streamId = nextStreamId;
+        streams[streamId] = Types.Stream({
+        remainingBalance : _deposit,
+        deposit : _deposit,
+        isEntity : true,
+        ratePerSecond : ratePerSecond,
+        recipient : _recipient,
+        sender : msg.sender,
+        startTime : _startTime,
+        stopTime : _stopTime,
+        tokenAddress : _tokenAddress
+        });
 
         return streamId;
     }
