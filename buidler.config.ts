68ad2be22667458a84ea6083bcb0d74f3aca524a
lwtsn <>
2020-10-21 22:41:13 +0800

    add deployment

--- buidler.config.ts
+++ buidler.config.ts
@@ -3,6 +3,7 @@ import {BuidlerConfig, usePlugin} from "@nomiclabs/buidler/config";
 usePlugin("@nomiclabs/buidler-ethers");
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@openzeppelin/buidler-upgrades");
+usePlugin("buidler-deploy");
 
 const config: BuidlerConfig = {
   solc: {
@@ -14,6 +15,19 @@ const config: BuidlerConfig = {
     tests: "./test",
     cache: "./cache",
     artifacts: "./artifacts",
+  },
+  defaultNetwork: "rinkeby",
+  networks: {
+    buidlerevm: {},
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/73bd0ea4c5d64e248551358ec2f1a8c3",
+      accounts: [""],
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+    proxyOwner: 1,
+    admin: "0xa4b9D4D9f0Bd2c16A2eb349F7C26aD508D06A895",
   },
 };
 
